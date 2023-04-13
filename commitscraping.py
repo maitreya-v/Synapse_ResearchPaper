6cfd06914c4009af1e4802dfea0f8d3898c491f0
maitreya-v <maitreya.vaghulade@gmail.com>
2023-04-14 01:15:31 +0530

    url goes back and forth + i am tired

--- commitscraping.py
+++ commitscraping.py
@@ -4,14 +4,14 @@ from selenium.webdriver.common.by import By
 
 commit_list= []
 
-url='https://github.com/facebook/react/commits/main'
+new_url='https://github.com/facebook/react/commits/main'
 
 driver = webdriver.Chrome()
 
 # driver.get(url)
 
 list_of_commits=[]
-new_url=None
+# new_url=None
 # older_btn_href=older_btn.__getattribute__('href')
 total_commits=0
 def get_commits(url):
@@ -22,9 +22,6 @@ def get_commits(url):
     for btn in button:
        btn.click()
     
-    date = driver.find_elements(By.CLASS_NAME,'f5,text-normal')
-    for d in date:
-        print(d.text)
         
         
     commits = driver.find_elements(By.CLASS_NAME,'ws-pre-wrap')
@@ -35,14 +32,22 @@ def get_commits(url):
     older_btn = driver.find_elements(By.XPATH,'.//*[@id="repo-content-pjax-container"]/div/div[3]/div/a') 
     return older_btn[0].get_attribute('href')
     # older_btn_url=driver.find_elements(By.TAG_NAME,'   
-new_url=get_commits('https://github.com/facebook/react/commits/main')    
+# new_url=get_commits(url)    
 
-# while(new_url!=None):
-for i in range(1):
+while(new_url!=None):
+# for i in range(1):
     new_url=get_commits(new_url)
     
-# print(len(list_of_commits)) 
-# print(list_of_commits)   
+print(len(list_of_commits)) 
+print(list_of_commits)   
 
     
 driver.close()    
+
+
+dataDf = pd.DataFrame()
+dataDf["commits"] = list_of_commits
+print(dataDf)
+
+#this will return a CSV file
+dataDf.to_csv(r'react_commits.csv', index=False) 
