da4ada5350b9963f52981b69dc4c2dd346f2c4ab
maitreya-v <102204411+maitreya-v@users.noreply.github.com>
2023-04-14 20:02:32 +0530

    fixed newer button and older button conflict,overall process slowed down due to increasing the number of for loops

--- commitscraping.py
+++ commitscraping.py
@@ -3,8 +3,10 @@ import pandas as pd
 from selenium.webdriver.common.by import By
 
 commit_list= []
-
+# hello
 new_url='https://github.com/facebook/react/commits/main'
+# new_url='https://github.com/facebook/react-native/commits/main'
+# new_url='https://github.com/angular/angular/commits/main'
 
 driver = webdriver.Chrome()
 
@@ -14,23 +16,41 @@ list_of_commits=[]
 # new_url=None
 # older_btn_href=older_btn.__getattribute__('href')
 total_commits=0
+page_number=0
 def get_commits(url):
+    global total_commits
+    global page_number
+    
+    
     url=url
     driver.get(url)
     button = driver.find_elements(By.CLASS_NAME,'ellipsis-expander')
 
     for btn in button:
-       btn.click()
+        # if btn.exists():
+            btn.click()
+        # else:
+            # pass
+    
     
         
         
     commits = driver.find_elements(By.CLASS_NAME,'ws-pre-wrap')
+    page_number+=1
+    print(page_number)
     # print(commits[0].text)
     for commit in commits:
+        # if commit.exists():
+            list_of_commits.append(commit.text)
+        # else:
+            # pass 
 
-       list_of_commits.append(commit.text)
-    older_btn = driver.find_elements(By.XPATH,'.//*[@id="repo-content-pjax-container"]/div/div[3]/div/a') 
-    return older_btn[0].get_attribute('href')
+    
+    all_btn=driver.find_elements(By.CLASS_NAME,'BtnGroup-item')
+    for btn in all_btn:
+        if btn.text=='Older':
+            older_btn_link=btn.get_attribute('href')
+    return older_btn_link
     # older_btn_url=driver.find_elements(By.TAG_NAME,'   
 # new_url=get_commits(url)    
 
